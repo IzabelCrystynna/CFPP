27bd47d3ad292c41379bb2878f2aab74825d23f7
izabel <izabel02cristina@gmail.com>
2021-02-23 11:10:12 -0300

    adaptação do template

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('layout.inicio');
+})->name('index');
+Route::get('/login', function () {
+    return view('layout.login');
+})->name('login');
+Route::get('/cadastro', function () {
+    return view('layout.cadastro');
+})->name('cadastro');
 
-Route::get('/home', function () {
-    return view('Cadastro_Compra');
-});
+
